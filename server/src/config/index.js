e509c3378811bb4afa0dcb0f08398e612b118762
AaronW <zmtzwb@gmail.com>
2023-03-06 15:33:38 +0800

    refactor .env file config loading

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,23 +1,22 @@
+const envSchema = require('env-schema')
 const path = require("path");
-const fs = require('fs');
-const dotenv = require('dotenv');
 
-let confName = ".env";
-const loadConfig = (env = '') => {
-  const basePath = process.cwd();
-
-  env && (confName = confName.concat('.', env));
-
-  const confPath = path.resolve(process.cwd(), confName);
-
-  if (!fs.existsSync(confPath)) {
-    throw new Error("Local ENV file could not found!")
+const schema = {
+  type: 'object',
+  required: ['API_KEY'],
+  properties: {
+    PORT: { type: "number", default: 3000 },
+    API_KEY: { type: "string" },
+    ORG: { type: "string", default: "" },
   }
-
-  const confStr = fs.readFileSync(confPath).toString('utf-8');
-  const conf = dotenv.parse(confStr);
-
-  return Object.assign({}, conf);
 }
 
-module.exports = loadConfig()
+let confName = ".env";
+process.env.NODE_ENV && (confName = confName.concat('.', process.env.NODE_ENV));
+
+module.exports = envSchema({
+  schema,
+  dotenv: {
+    path: path.resolve(process.cwd(), confName),
+  },
+});
