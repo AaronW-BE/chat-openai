fbb3f46fc65ebbf94681c521c957908dbcb26a7e
AaronW <zmtzwb@gmail.com>
2023-03-09 10:22:07 +0800

    add reauth support

--- front/utils/request.js
+++ front/utils/request.js
@@ -1,18 +1,38 @@
 const BASEURL = "http://127.0.0.1:3000";
-export const GET = (url, data = {}, headers = {}) => {
 
+export const R = (url, method, query = {}, data = {}, headers = {}) => {
   let token = wx.getStorageSync('token');
   if (token.content) {
     headers.Authorization = "Bearer " + token.content;
   }
 
+  if (method.toUpperCase() === "GET") {
+    data = query;
+  }
+
+  let MAX_FAIL_RETRY_COUNT = 3;
+  let failCount = 0;
   return new Promise((resolve, reject) => {
     wx.request({
       url: BASEURL + url,
-      method: "GET",
+      method,
       data,
       header: headers,
-      success(res) {
+      async success(res) {
+        if (res.statusCode === 401) {
+          if (failCount < MAX_FAIL_RETRY_COUNT) {
+            failCount++;
+          } else {
+            console.warn("beyond max retry times");
+            reject(res.data);
+          }
+
+          let app = getApp();
+          await app.auth();
+
+          // resend
+          await R(url, method, query, data, headers);
+        }
         resolve(res.data);
       },
       fail(res) {
@@ -22,26 +42,10 @@ export const GET = (url, data = {}, headers = {}) => {
   })
 }
 
+export const GET = (url, data = {}, headers = {}) => {
+  return R(url, "GET", data, {}, headers);
+}
+
 export const POST = (url, data, headers = {}) => {
-  return new Promise((resolve, reject) => {
-    // set auth header
-
-    let token = wx.getStorageSync('token');
-    if (token.content) {
-      headers.Authorization = "Bearer " + token.content;
-    }
-
-    wx.request({
-      url: BASEURL + url,
-      method: "POST",
-      data,
-      header: headers,
-      success(res) {
-        resolve(res.data);
-      },
-      fail(res) {
-        reject(res);
-      }
-    })
-  })
+  return R(url, "POST", data, {}, headers);
 }
