ad17a06707542ce78caf49d061cfa63bc8b8acea
AaronW <zmtzwb@gmail.com>
2023-03-09 10:21:49 +0800

    add update support

--- front/app.js
+++ front/app.js
@@ -7,6 +7,22 @@ App({
 
     // TODO modify later
     this.auth();
+
+    this.checkUpdate();
+  },
+  checkUpdate() {
+    let um = wx.getUpdateManager()
+    um.onUpdateReady(function () {
+      wx.showModal({
+        title: "更新提示",
+        content: "新版本已准备好，是否重启以更新",
+        success(res) {
+          if (res.confirm) {
+            um.applyUpdate();
+          }
+        }
+      })
+    });
   },
   // for login & get user token
   auth() {
